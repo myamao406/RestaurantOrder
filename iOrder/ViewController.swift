92497e0ac4f631ae0e757e3471b2f9c2f473bcf9
やまおまもる <mamoru@yamaomamoru-no-macbook-pro.local>
2016-06-25 11:08:58 +0900

    Initial Commit

---
+++ iOrder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOrder
+//
+//  Created by 山尾守 on 2016/06/25.
+//  Copyright © 2016年 山尾守. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
